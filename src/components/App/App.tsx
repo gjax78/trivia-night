bae64296fe0d06e57b0724d5cf00628b0b987714
Casey Halstead <catherineclosee@gmail.com>
2022-04-10 16:13:17 -0500

    Push for learning

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,57 +1,24 @@
-import { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import fetchData from '../../apiCalls'
-import { isThisTypeNode } from 'typescript';
+import { getParsedCommandLineOfConfigFile, isThisTypeNode } from 'typescript';
 import { render } from '@testing-library/react';
 import Questions from '../Questions/Questions'
 
-import data from '../../data';
+const App = () => {
+  const [questions, setQuestions] = useState<any>([]);
 
-interface Props {}
+useEffect(() => {
+  fetchData.getData('https://the-trivia-api.com/questions?categories=arts_and_literature&limit=20')
+  .then(data => setQuestions(data))
+  }, [])
 
-interface State {
-  questions: Array<any>
-}
-
-interface Question {
-  category: string,
-  id: string,
-  correctAnswer: string,
-  incorrectAnswers: any,
-  question: string,
-  type: string,
-  tags: any,
-  difficulty: string
-}
-
-class App extends Component<Props, State> {
-  constructor(props: any) {
-    super(props);
-    this.state = {
-      questions: []
-    }
-  }
-  
-  // fetchData = () => {
-  //   fetchData.getData('https://the-trivia-api.com/questions?categories=arts_and_literature&limit=20')
-  //   .then(data => this.setState({questions: data}))
-  //   console.log("first")
-  //   }
-
-  // componentDidMount = () => this.fetchData();
-
-  componentDidMount = () => this.setState({questions: data});
-
-  render() {
-    {console.log(data)}
-    return (
+  return (
       <div className="App">
         <p>yo</p> 
-        <Questions questions={this.state.questions}/>
+        <Questions questions={questions}/>
       </div>
-    );
-
-  }
+    )
 }
 
 export default App;
