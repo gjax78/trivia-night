18cee37523762125210702196cdc52f14bbb911c
Geena Jackson <gjacks303@gmail.com>
2022-04-12 10:03:26 -0600

    Add div className

--- src/components/Questions/Questions.tsx
+++ src/components/Questions/Questions.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import QuestionCard from '../QuestionCard/QuestionCard'
 import { QuestionData as QuestionData} from '../App/App'
 
-const Questions = ({ questions }: QuestionsData) => {
+const Questions = ({ questions }: QuestionData) : JSX.Element => {
   const questionCards = questions.map(question => {
     if(!question.difficulty) {
       question.difficulty = "easy"
     }
     return (
-        <div className='ideas-container'>
+        <div className='questions-container'>
           <QuestionCard
             category={question.category}
             id={question.id}
@@ -24,7 +24,7 @@ const Questions = ({ questions }: QuestionsData) => {
     )
   })
   return (
-    <div>
+    <div className='questions-container'>
       {questionCards}
       {/* <button onClick={() => saveQuestions(id)}>Save</button> */}
     </div>
