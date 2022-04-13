fafe56b8cd27db9041dc9918fabefb4954382e81
Geena Jackson <gjacks303@gmail.com>
2022-04-12 18:16:01 -0600

    Refactor apiCalls spacing

--- src/apiCalls.tsx
+++ src/apiCalls.tsx
@@ -1,14 +1,14 @@
 const fetchData = {
-    getData(url : string) {
-        return fetch(url)
-            .then(response => {
-                if(!response.ok) {
-                    throw new Error("error")
-                } else {
-                    return response.json()
-                }
-            })
-    }
+  getData(url : string) {
+    return fetch(url)
+      .then(response => {
+        if(!response.ok) {
+          throw new Error("error")
+        } else {
+          return response.json()
+        }
+      })
+  }
 }
 
 export default fetchData;
