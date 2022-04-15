9b1e1586b72477f8d971161b21408517b872abdc
Sam Mistele <smmstl0@gmail.com>
2022-04-15 19:01:05 -0400

    Add Link to questions page

--- src/components/Categories/Categories.tsx
+++ src/components/Categories/Categories.tsx
@@ -1,48 +1,69 @@
-import React, { useState, useEffect } from 'react'
-import { QuestionsType as CategoriesDataType } from '../App/App'
-import fetchData from '../../apiCalls'
-
+import React, { useState, useEffect } from 'react';
+import { QuestionsType as CategoriesDataType } from '../App/App';
+import fetchData from '../../apiCalls';
+import { Route, Link, Switch } from 'react-router-dom';
+import Questions from '../Questions/Questions';
 
 interface Card {
   onClick: React.MouseEvent<HTMLButtonElement>;
 }
 
-const Categories = ( {handleClick} ) => {
+const Categories = ( { fetchCat } ) => {
 
   return (
     <div className='button-container'>
+    <Link to='/questions'>
       <button
       className='arts-and-literature'
       value={'arts_and_literature'}
-      onClick={(event) => handleClick(event, 'arts_and_literature')}>Arts & Literature</button>
+      onClick={() => fetchCat('arts_and_literature')}>Arts & Literature</button>
+    </Link>
+    <Link to='/questions'>
       <button className='film-and-tv'
       value={'film_and_tv'}
-      onClick={(event) => handleClick(event, 'film_and_tv')}>Film & TV</button>
+      onClick={() => fetchCat('film_and_tv')}>Film & TV</button>
+    </Link>
+    <Link to='/questions'>
       <button className='food-and-drink'
       value={'food_and_drink'}
-      onClick={(event) => handleClick(event, 'food_and_drink')}>Food & Drink</button>
+      onClick={() => fetchCat('food_and_drink')}>Food & Drink</button>
+    </Link>
+    <Link to='/questions'>
       <button
       className='general-knowledge'
       value={'general_knowledge'}
-      onClick={(event) => handleClick(event, 'general-knowledge')}>General Knowledge</button>
+      onClick={() => fetchCat('general-knowledge')}>General Knowledge</button>
+    </Link>
+    <Link to='/questions'>
       <button className='geography'
       value={'geography'}
-      onClick={(event) => handleClick(event, 'geography')}>Geography</button>
+      onClick={() => fetchCat('geography')}>Geography</button>
+    </Link>
+    <Link to='/questions'>
       <button className='history'
       value={'history'}
-      onClick={(event) => handleClick(event, 'history')}>History</button>
+      onClick={() => fetchCat('history')}>History</button>
+    </Link>
+    <Link to='/questions'>
       <button className='music'
       value={'music'}
-      onClick={(event) => handleClick(event, 'music')}>Music</button>
+      onClick={() => fetchCat('music')}>Music</button>
+    </Link>
+    <Link to='/questions'>
       <button className='science'
       value={'science'}
-      onClick={(event) => handleClick(event, 'science')}>Science</button>
+      onClick={() => fetchCat('science')}>Science</button>
+    </Link>
+    <Link to='/questions'>
       <button className='society-and-culture'
       value={'society_and_culture'}
-      onClick={(event) => handleClick(event, 'society_and_culture')}>Society & Culture</button>
+      onClick={() => fetchCat('society_and_culture')}>Society & Culture</button>
+    </Link>
+    <Link to='/questions'>
       <button className='sport-and-leisure'
       value={'sport_and_leisure'}
-      onClick={(event) => handleClick(event, 'sport_and_leisure')}>Sports & Leisure</button>
+      onClick={() => fetchCat('sport_and_leisure')}>Sports & Leisure</button>
+    </Link>
     </div>
   )
 }
