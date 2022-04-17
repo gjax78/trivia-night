f993f85b2ce35dd0de8ce4507828ff461a759d5d
Geena Jackson <gjacks303@gmail.com>
2022-04-17 14:35:27 -0600

    Refactor main page testing

--- cypress/integration/MainPage_spec.ts
+++ cypress/integration/MainPage_spec.ts
@@ -13,62 +13,62 @@ describe('Main Page', () => {
   it('should have a category button for Arts & Literature', () => {
     cy.visit('http://localhost:3000/')
     cy.get('*[class^="arts-and-literature"]')
-      .should('have.text', 'Arts & LiteratureArts & Literature')
+      .should('have.text', 'Arts & Literature')
   })
 
   it('should have a category button for Film & TV', () => {
     cy.visit('http://localhost:3000/')
     cy.get('*[class^="film-and-tv"]')
-      .should('have.text', 'Film & TVFilm & TV')
+      .should('have.text', 'Film & TV')
 
   })
 
   it('should have a category button for Food & Drink', () => {
     cy.visit('http://localhost:3000/')
     cy.get('*[class^="food-and-drink"]')
-      .should('have.text', 'Food & DrinkFood & Drink')
+      .should('have.text', 'Food & Drink')
 
   })
 
   it('should have a category button for General Knowledge', () => {
     cy.visit('http://localhost:3000/')
     cy.get('*[class^="general-knowledge"]')
-      .should('have.text', 'General KnowledgeGeneral Knowledge')
+      .should('have.text', 'General Knowledge')
   })
 
   it('should have a category button for Geography', () => {
     cy.visit('http://localhost:3000/')
     cy.get('*[class^="geography"]')
-      .should('have.text', 'GeographyGeography')
+      .should('have.text', 'Geography')
   })
 
   it('should have a category button for History', () => {
     cy.visit('http://localhost:3000/')
     cy.get('*[class^="history"]')
-      .should('have.text', 'HistoryHistory')
+      .should('have.text', 'History')
   })
 
   it('should have a category button for Music', () => {
     cy.visit('http://localhost:3000/')
     cy.get('*[class^="music"]')
-      .should('have.text', 'MusicMusic')
+      .should('have.text', 'Music')
   })
 
   it('should have a category button for Science', () => {
     cy.visit('http://localhost:3000/')
     cy.get('*[class^="science"]')
-      .should('have.text', 'ScienceScience')
+      .should('have.text', 'Science')
   })
 
   it('should have a category button for Society & Culture', () => {
     cy.visit('http://localhost:3000/')
     cy.get('*[class^="society-and-culture"]')
-      .should('have.text', 'Society & CultureSociety & Culture')
+      .should('have.text', 'Society & Culture')
   })
 
   it('should have a category button for Sports & Leisure', () => {
     cy.visit('http://localhost:3000/')
     cy.get('*[class^="sport-and-leisure"]')
-      .should('have.text', 'Sports & LeisureSports & Leisure')
+      .should('have.text', 'Sports & Leisure')
   })
 })
