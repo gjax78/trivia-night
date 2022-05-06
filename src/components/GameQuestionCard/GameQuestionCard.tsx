8af7d6438cd6904c8247c96bdb23842869e7df61
Geena Jackson <gjacks303@gmail.com>
2022-05-05 22:23:40 -0600

    Refactor styling

--- src/components/GameQuestionCard/GameQuestionCard.tsx
+++ src/components/GameQuestionCard/GameQuestionCard.tsx
@@ -6,11 +6,15 @@ const GameQuestionCard = ({category, question, correctAnswer, incorrectAnswers, 
 
   return (
     <div className='game-card'>
-      <h2>{category}</h2>
-      <h4>{question}</h4>
-      <p>CORRECT ANSWER: {correctAnswer}</p>
-      <p>INCORRECT ANSWERS: {incorrectAnswers.join(', ')}</p>
-      <p>DIFFICULTY: {difficulty}</p>
+      <h2>Category: <br></br> {category}</h2>
+      <h4><u>QUESTION:</u><br></br> 
+        {question}</h4>
+      <p><u>CORRECT ANSWER:</u> <br></br>
+        {correctAnswer}</p>
+      <p><u>INCORRECT ANSWERS: </u><br></br>
+        {incorrectAnswers.join(', ')}</p>
+      <p><u>DIFFICULTY: </u><br></br>
+        {difficulty}</p>
 
       <button
         className='delete-button'
